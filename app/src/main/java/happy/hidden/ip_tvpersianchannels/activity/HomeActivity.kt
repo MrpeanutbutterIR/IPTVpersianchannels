5bda401b442c1bec70cbb5852834c6deabd59bc1
happyhidden <info@happyhidden.happyhidden>
2020-03-30 15:30:02 +0430

    start again change mine

---
+++ app/src/main/java/happy/hidden/ip_tvpersianchannels/activity/HomeActivity.kt
@@ -0,0 +1,13 @@
+package happy.hidden.ip_tvpersianchannels.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import happy.hidden.ip_tvpersianchannels.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
